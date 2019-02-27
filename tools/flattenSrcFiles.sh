b8e408723cdbb6ef8ac8fa50632fb467a8881ef0
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-02-27 18:53:32 +0000

    bash flattener actually apped

---
+++ tools/flattenSrcFiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+srcFolder=$1
+destFolder=$2
+fileExt=$3
+
+srcFiles=$(find $srcFolder -name "*.$fileExt")
+mkdir -p "$destFolder"
+
+for srcFile in $srcFiles
+do
+    fileName=$(basename "$srcFile")
+    cp -f $srcFile "$destFolder/$filename"
+done
+
+
