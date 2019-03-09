b70c8bd62bd5c31b3d51ecac3163f29fe3c82c6b
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-09 16:07:22 +0000

    fixed if princode

--- src/ast/ifStatement.cpp
+++ src/ast/ifStatement.cpp
@@ -2,5 +2,5 @@
 
 void IfStatement::printCode(std::ostream &os) const
 {
-    os << "if (" << branches[0] << " )" << branches[1] << "else" << branches[2]; //the scopeBlock printCode is repsonsible for printing the { } with new lines
+    os << "if (" << branches[0] << ")" << branches[1] << "else" << branches[2]; //the scopeBlock printCode is repsonsible for printing the { } with new lines
 }
