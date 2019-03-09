d207330e9d91818cb2ad2d7bca997b219b84c453
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-09 15:47:44 +0000

    done for loop skeleton

---
+++ include/ast/loops/forLoop.hpp
@@ -0,0 +1,16 @@
+#ifndef forLoop_hpp
+#define forLoop_hpp
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <memory>
+#include "statement.hpp"
+
+class ForLoop : Statement //will have branch[0] initializatino,, branch[1] condition, branch[2] increment/decr, branch[3] execution block
+{
+  protected:
+    virtual void printCode(std::ostream &os) const = 0;
+};
+
+#endif
--- playground/ast/helloWorld.c
+++ playground/ast/helloWorld.c
@@ -1,6 +1,13 @@
 // #include <stdio.h>
 int main()
 {
+   int zzz = 0;
+   int i = 0;
+   for(i = 0; i < 50; i++)
+   {
+      zzz++;
+   }
+   
 
 float (*op[42])(float, float); //this gets converted to 1 vardecl node 
 
