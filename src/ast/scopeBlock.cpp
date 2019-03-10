654102893d01e6bd3ae26879d223e3b536d90b0e
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-10 01:55:23 +0000

    fixed princ functiont for scope block

--- src/ast/scopeBlock.cpp
+++ src/ast/scopeBlock.cpp
@@ -5,7 +5,7 @@ void ScopeBlock::printCode(std::ostream &os) const
     os << "\n{\n";
     for(size_t i = 0; i < branches.size(); i++)
     {
-        os << branches[i] << << ";\n";
+        os << branches[i] << ";\n";
     }
     os << "\n}" << std::endl; //to flush the buffer
 }
