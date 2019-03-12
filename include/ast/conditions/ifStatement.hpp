8bfd98a3044fc735b5e9e347635d8cca384372bf
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-12 01:49:52 +0000

    changed if statement scope block getter return type

--- include/ast/conditions/ifStatement.hpp
+++ include/ast/conditions/ifStatement.hpp
@@ -6,13 +6,14 @@
 #include <iostream>
 #include <memory>
 #include "statement.hpp"
+#include "scopeBlock.hpp"
 
 class IfStatement : public Statement //will have branch[0] condition, branch[1] if block, and branch[2] else block
 {
   protected:
     StatementPtr getCondition() const;
-    StatementPtr getIfScopeBlock() const;
-    StatementPtr getElseScopeBlock() const;
+    ScopeBlock getIfScopeBlock() const;
+    ScopeBlock getElseScopeBlock() const;
     
     void printC(std::ostream &os) const override;
 };
--- include/ast/functions/functionDefinition.hpp
+++ include/ast/functions/functionDefinition.hpp
@@ -1,14 +1,16 @@
-#ifndef functionDeclaration_hpp
-#define functionDeclaration_hpp
+#ifndef functionDefinition_hpp
+#define functionDefinition_hpp
 
 #include <iostream>
 #include "statement.hpp"
 #include "functionParameterList.hpp"
+#include "scopeBlock.hpp"
 
 class FunctionDeclaration : public Statement 
 {
   public:
     FunctionParameterList getParameters() const;
+    ScopeBlock getScopeBlock() const;
     void printC(std::ostream &os) const override;
 };
 
