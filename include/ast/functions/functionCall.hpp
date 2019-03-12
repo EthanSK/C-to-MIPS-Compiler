2d120de7253f56734869c3f94b86768202a6267a
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-12 01:54:12 +0000

    started on functionCall

--- include/ast/functions/functionCall.hpp
+++ include/ast/functions/functionCall.hpp
@@ -1,13 +1,11 @@
-#ifndef floatLiteral_hpp
-#define floatLiteral_hpp
+#ifndef functionCall_hpp
+#define functionCall_hpp
 
 #include <iostream>
 #include "literal.hpp"
 
-class FloatLiteral : public Literal
+class FunctionCall : public 
 {
-  private:
-    float value;
   public:
     float getValue() const;
     void printC(std::ostream &os) const override;
