1649ed1420cbc74ce092a700489f014be2b971d4
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-13 01:27:43 +0000

    started adding type to functions

--- src/ast/functions/functionCall.cpp
+++ src/ast/functions/functionCall.cpp
@@ -1,8 +1,8 @@
 #include "functionCall.hpp"
 
-FunctionCall::FunctionCall(StatementPtr parameters){
+FunctionCall::FunctionCall(std::string name, StatementPtr parameters) : Function(name) {
     branches[0] = parameters;
-}
+}  
 StatementPtr FunctionCall::getParameters() const{
     return branches[0];
 }
