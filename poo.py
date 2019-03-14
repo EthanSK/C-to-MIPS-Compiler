f07c0aa5c574769cdbb9079722ac6056a9727a1d
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-14 18:17:57 +0000

    prints python to file properly

---
+++ poo.py
@@ -0,0 +1,9 @@
+
+x = 2
+if (10 >= (7 + 7.7)):
+    ("pink" == "purple")
+else:
+    if (3 <= (1.374 + 7.7)):
+        ("lost" != "dizzy")
+    else:
+        x = 42
--- src/main.cpp
+++ src/main.cpp
@@ -29,10 +29,11 @@ int main(int argc, char *argv[])
     std::cout << ast << std::endl;
     ast->writePrintCToFile();
 
+    std::cout << "\n\nPYTHON CODE\n======================\n";
+    ast->generatePython(std::cout, context);
+
     if (std::string(argv[1]) == "--translate")
     {
-        std::cout << "\n\nPYTHON CODE\n======================\n";
-        ast->generatePython(std::cout, context);
         ast->writePythonToFile(context, std::string(argv[4]));
         std::cout << std::endl;
     }
@@ -57,24 +58,14 @@ StatementPtr generateTestAST()
 
 StatementPtr generateTestFragment()
 {
-    StatementPtr ast = new ScopeBlock({
-        new BinaryAssignment(new VariableDeclaration(new PrimitiveType(PrimitiveType::PrimitiveTypeEnum::_int), "x"), new IntegerLiteral(2)),
-        new IfElseStatement(
-            new BinaryGreaterThanOrEqualTo(new DoubleLiteral(10), new BinaryAdd(new IntegerLiteral(7), new FloatLiteral(7.7f))),
-            new ScopeBlock({
-                new BinaryIsEqualTo(new StringLiteral("pink"), new StringLiteral("purple"))
-            }),
-            new ScopeBlock({
-                new IfElseStatement(
-                    new BinaryLessThanOrEqualTo(new DoubleLiteral(3), new BinaryAdd(new DoubleLiteral(1.374), new FloatLiteral(7.7f))),
-                    new ScopeBlock({
-                        new BinaryIsNotEqualTo(new StringLiteral("lost"), new StringLiteral("dizzy"))
-                    }),
-                    new ScopeBlock({
-                            new BinaryAssignment(new VariableReference("x"), new IntegerLiteral(42))
-                    }))
-            }))
-    });
+    StatementPtr ast = new ScopeBlock({new BinaryAssignment(new VariableDeclaration(new PrimitiveType(PrimitiveType::PrimitiveTypeEnum::_int), "x"), new IntegerLiteral(2)),
+                                       new IfElseStatement(
+                                           new BinaryGreaterThanOrEqualTo(new DoubleLiteral(10), new BinaryAdd(new IntegerLiteral(7), new FloatLiteral(7.7f))),
+                                           new ScopeBlock({new BinaryIsEqualTo(new StringLiteral("pink"), new StringLiteral("purple"))}),
+                                           new ScopeBlock({new IfElseStatement(
+                                               new BinaryLessThanOrEqualTo(new DoubleLiteral(3), new BinaryAdd(new DoubleLiteral(1.374), new FloatLiteral(7.7f))),
+                                               new ScopeBlock({new BinaryIsNotEqualTo(new StringLiteral("lost"), new StringLiteral("dizzy"))}),
+                                               new ScopeBlock({new BinaryAssignment(new VariableReference("x"), new IntegerLiteral(42))}))}))});
 
     return ast;
 }
