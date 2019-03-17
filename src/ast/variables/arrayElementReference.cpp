411cdbdd3174eb22e09ca092027bbcd0701c146f
QFSW <yusuf.ismail1024@gmail.com>
2019-03-17 20:35:07 +0000

    Changed array part of array element access to a subtree

--- src/ast/variables/arrayElementReference.cpp
+++ src/ast/variables/arrayElementReference.cpp
@@ -1,16 +1,22 @@
 #include "arrayElementReference.hpp"
 
-ArrayElementReference::ArrayElementReference(std::string name, StatementPtr index) : VariableReference(name)
+ArrayElementReference::ArrayElementReference(StatementPtr arr, StatementPtr index)
 {
+    branches.push_back(arr);
     branches.push_back(index);
 }
 
-StatementPtr ArrayElementReference::getIndex() const
+StatementPtr ArrayElementReference::getArr() const
 {
     return branches[0];
 }
 
+StatementPtr ArrayElementReference::getIndex() const
+{
+    return branches[1];
+}
+
 void ArrayElementReference::printC(std::ostream &os) const
 {
-    os << _name << "[" << getIndex() << "]";
+    os << getArr() << "[" << getIndex() << "]";
 }
