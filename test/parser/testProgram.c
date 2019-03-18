b6fc2a1e8aba4c96696e11d06774787b6f1e411e
QFSW <yusuf.ismail1024@gmail.com>
2019-03-18 23:42:18 +0000

    == now works

--- test/parser/testProgram.c
+++ test/parser/testProgram.c
@@ -1,13 +1,10 @@
-extern int someInt[2 * 5];
-int a1 = 1, a2 = 2, a3 = 3;
-
 int factorial(int n)
 {
-    if (n < 1) { return 1; }
+    if (n == 0) { return 1; }
     else { return n * factorial(n - 1); }
 }
 
 int main()
 {
-    return 0;
+    return factorial(5);
 }
