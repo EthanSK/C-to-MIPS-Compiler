50e5ae31b1b60af45825a0cce5f77d99f6f98d19
QFSW <yusuf.ismail1024@gmail.com>
2019-03-18 20:42:25 +0000

    Added second constructor to VariableDeclarator

--- include/ast/declarators/variableDeclarator.hpp
+++ include/ast/declarators/variableDeclarator.hpp
@@ -2,11 +2,14 @@
 #define variableDeclarator_hpp
 
 #include "declarator.hpp"
+#include "identifier.hpp"
+#include <string>
 
 class VariableDeclarator : public Declarator
 {
 public:
     VariableDeclarator(StatementPtr identifier);
+    VariableDeclarator(std::string identifier) : VariableDeclarator(new Identifier(identifier)) { }
     void generatePython(std::ostream &os, PythonContext &context, int scopeDepth = 0) const override;
     std::string getIdentifierName() const override;
 
