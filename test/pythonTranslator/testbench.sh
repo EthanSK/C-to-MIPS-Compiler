71bab9fb47abee2ac04b21950b8d877067433067
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-18 23:00:07 +0000

    testbench outputs to csv now

--- test/pythonTranslator/testbench.sh
+++ test/pythonTranslator/testbench.sh
@@ -14,10 +14,12 @@ if [[ ! -f bin/c_compiler ]] ; then
 fi
 
 cwd="test/pythonTranslator"
-
 input_dir="$cwd/tests"
-
 output_dir="$cwd/output"
+output_file="$output_dir/_summary.csv"
+
+rm -f $output_file
+rm -rf $output_dir
 mkdir -p ${output_dir}
 
 for c_file in ${input_dir}/*.c ; do
@@ -28,29 +30,33 @@ for c_file in ${input_dir}/*.c ; do
     
     # Run the reference C version
     $output_dir/$base
-    REF_C_OUT=$?
+    ref_c_exit_code=$?
     
     # Run the reference python version
     python3 ${input_dir}/$base.py
-    REF_P_OUT=$?
+    ref_py_exit_code=$?
     
     if [[ ${have_compiler} -eq 0 ]] ; then
-        
+        translated_python_file="${base}Got.py"
         # Create the DUT python version by invoking the compiler with translation flags
-        $compiler --translate $c_file -o ${output_dir}/$base-got.py
+        $compiler --translate $c_file -o ${output_dir}/$translated_python_file
         
         # Run the DUT python version
-        python ${output_dir}/$base-got.py
-        GOT_P_OUT=$?
+        python ${output_dir}/$translated_python_file
+        got_py_exit_code=$?
     fi
     
-    if [[ $REF_C_OUT -ne $REF_P_OUT ]] ; then
-        echo "$base, REF_FAIL, Expected ${REF_C_OUT}, got ${REF_P_OUT}"
-    elif [[ ${have_compiler} -ne 0 ]] ; then
-        echo "$base, Fail, No C compiler/translator"
-    elif [[ $REF_C_OUT -ne $GOT_P_OUT ]] ; then
-        echo "$base, Fail, Expected ${REF_C_OUT}, got ${GOT_P_OUT}"
+    
+    if [[ $ref_c_exit_code -ne $ref_py_exit_code ]] ; then
+        result="$base, REF_FAIL, Expected ${ref_c_exit_code}, got ${ref_py_exit_code}" #something is wrong with the test
+        elif [[ ${have_compiler} -ne 0 ]] ; then
+        result="$base, Fail, No C compiler/translator"
+        elif [[ $ref_c_exit_code -ne $got_py_exit_code ]] ; then
+        result="$base, Fail, Expected ${ref_c_exit_code}, got ${got_py_exit_code}" #something is wrong with our compiler
     else
-        echo "$base, Pass"
+        result="$base, Pass"
     fi
+    
+    csv_line="$result"
+    echo $csv_line >> $output_file
 done
