6f6e875d50d04c59d39d8d2460c74facf0c547e6
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-19 05:17:48 +0000

    sexy SEXY colours

--- test/compiler/src/runTests.sh
+++ test/compiler/src/runTests.sh
@@ -40,13 +40,24 @@ mkdir -p $log_dir
 $cwd/moveTests.sh #so we can run tests from final dir for security
 
 
+#Colours!!
+no_colour='\033[0m'       # Colour Reset
+black='\033[0;30m'        # Black
+red='\033[0;31m'          # Red
+green='\033[0;32m'        # Green
+yellow='\033[0;33m'       # Yellow
+blue='\033[0;34m'         # Blue
+purple='\033[0;35m'       # Purple
+cyan='\033[0;36m'         # Cyan
+white='\033[0;37m'        # White
+
 
 
 #============================================FUNCTIONALITY================================================
 
 #run test
 for c_file in ${test_cases_dir}/*.c ; do #use test_cases_dir here to enforce correctly added tests only
-
+    
     [[ $c_file == *_driver.c ]] && continue #ignore the driver files
     base_name=$(echo $c_file | sed -E -e "s|${test_cases_dir}/([^.]+)[.]c|\1|g");
     driver_file="$deliverable_test_cases_dir/${base_name}_driver.c" #but here use deliverable_test_cases to enforce file loc
@@ -55,9 +66,10 @@ for c_file in ${test_cases_dir}/*.c ; do #use test_cases_dir here to enforce cor
     binary_out=$bin_dir/$base_name
     
     if [[ $is_verbose == 1 ]] ; then
-        printf "\n============= file info =============\n"
+        printf "${yellow}\n\n========================== $base_name ==========================\n\n${no_colour}"
+        printf "${purple}============= file info =============\n"
         echo "c_file: $c_file"; echo "base_name: $base_name"; echo "driver_file: $driver_file"; echo "asm_file: $asm_file"; echo "obj_file: $obj_file"; echo "binary_out: $binary_out"
-        printf "======================================\n\n"
+        printf "======================================\n${no_colour}"
     fi
     
     $c_compiler -S $c_file -o $asm_file
@@ -68,20 +80,23 @@ for c_file in ${test_cases_dir}/*.c ; do #use test_cases_dir here to enforce cor
     
     if [[ $exit_code == 0 ]]; then
         outcome="Pass"
+        colour=$green
     else
         outcome="Fail"
+        colour=$red
     fi
     
     csv_line="$base_name, exit code: $exit_code, $outcome"
     echo $csv_line >> $summary_file
     
     if [[ $is_verbose == 1 ]] ; then
-        printf "\n============= results ==============\n"
-        echo $csv_line
-        printf "======================================\n\n"
+        printf "${cyan}\n============= results ===============\n${no_colour}"
+        echo -e "${colour}$csv_line${no_colour}"
+        printf "${cyan}=====================================\n${no_colour}"
     fi
     
 done
+printf "${blue}\n\n========================== SUMMARY ==========================\n\n${no_colour}"
 
 #happens regardless of verbose, otherwise script would output nothing useful
 cat $summary_file | column -t -s, | grep -E --color=auto 'Fail|$$'
