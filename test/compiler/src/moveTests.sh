1483f2e5ffea8735eb636ba7b2f5379369627389
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-19 02:21:58 +0000

    fixed movetests

--- test/compiler/src/moveTests.sh
+++ test/compiler/src/moveTests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-test_cases_in_dir="test/deliverable/testCases"
+test_cases_in_dir="test/compiler/testCases"
 test_cases_out_dir="test_deliverable/test_cases"
 rm -rf $test_cases_out_dir
 mkdir -p $test_cases_out_dir
-cp -R $test_cases_in_dir/* $test_cases_out_dir
+cp -R $test_cases_in_dir $test_cases_out_dir
