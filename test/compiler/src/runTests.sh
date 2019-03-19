a75765815b3fd5a7b835cefc2bd6635bb0a37a20
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-19 04:55:54 +0000

    further. prettified.

--- test/compiler/src/runTests.sh
+++ test/compiler/src/runTests.sh
@@ -18,12 +18,12 @@ bin_dir="$compiler_test_dir/bin"
 log_dir="$compiler_test_dir/log"
 
 
-#tools
-c_compiler="mips-linux-gnu-gcc" #replace this with our compiler
-
-
 #files
 summary_file="$log_dir/_summary.csv"
+
+
+#tools
+c_compiler="mips-linux-gnu-gcc" #replace this with our compiler
 
 
 #flags
@@ -33,8 +33,10 @@ is_verbose=1
 #setup
 rm -rf $build_dir
 rm -rf $bin_dir
+rm -rf $log_dir
 mkdir -p $build_dir
 mkdir -p $bin_dir
+mkdir -p $log_dir
 $cwd/moveTests.sh #so we can run tests from final dir for security
 
 
@@ -46,7 +48,7 @@ $cwd/moveTests.sh #so we can run tests from final dir for security
 for c_file in ${test_cases_dir}/*.c ; do #use test_cases_dir here to enforce correctly added tests only
 
     [[ $c_file == *_driver.c ]] && continue #ignore the driver files
-    base_name=$(echo $c_file | sed -E -e "s|${test_cases_dir}/([^.]+)[.]c|\1|g"); 
+    base_name=$(echo $c_file | sed -E -e "s|${test_cases_dir}/([^.]+)[.]c|\1|g");
     driver_file="$deliverable_test_cases_dir/${base_name}_driver.c" #but here use deliverable_test_cases to enforce file loc
     asm_file=$build_dir/$base_name.s
     obj_file=$build_dir/$base_name.o
@@ -62,7 +64,24 @@ for c_file in ${test_cases_dir}/*.c ; do #use test_cases_dir here to enforce cor
     mips-linux-gnu-gcc -mfp32 -o $obj_file -c $asm_file
     mips-linux-gnu-gcc -mfp32 -static -o $binary_out $obj_file $driver_file
     qemu-mips $binary_out
-    
     exit_code=$?
-    echo "exit code: $exit_code"
+    
+    if [[ $exit_code == 0 ]]; then
+        outcome="Pass"
+    else
+        outcome="Fail"
+    fi
+    
+    csv_line="$base_name, exit code: $exit_code, $outcome"
+    echo $csv_line >> $summary_file
+    
+    if [[ $is_verbose == 1 ]] ; then
+        printf "\n============= results ==============\n"
+        echo $csv_line
+        printf "======================================\n\n"
+    fi
+    
 done
+
+#happens regardless of verbose, otherwise script would output nothing useful
+cat $summary_file | column -t -s, | grep -E --color=auto 'Fail|$$'
