1893df40484eb7bf16c386b5c956b7e3d3ac2c4c
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-19 14:09:27 +0000

    added playground

---
+++ playground/parser/generateASM.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+c_compiler="mips-linux-gnu-gcc" #replace this with our compiler
+gcc_flags="-mfp32 -std=c90 -ansi -pedantic"
+
+cwd="playground/parser"
+$c_compiler -S $cwd/test1.c -o $cwd/test1.s
