d07f842929674bc080ea55a52d67966665233c5e
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-19 00:48:02 +0000

    wrote some hlaf finished code

--- tools/flattenSrcFiles.sh
+++ tools/flattenSrcFiles.sh
@@ -10,6 +10,18 @@ for srcFile in $srcFiles
 do
     fileName=$(basename "$srcFile")
     rsync --update $srcFile "$destFolder/$fileName"
+    
+    
 done
 
 
+#half finished code
+# for destFile in $destFolder/*
+# do 
+#     #now delete files in tmp that aren't in the source any more
+#     if [[ ! -f "$srcFile" && -f "$destFolder/$fileName" ]]; then
+#         #file exists in tmp but not in source
+#         echo "file shouldn't be here: $destFolder/$fileName , $srcFile"
+#         #    rm -f $destFolder/$fileName
+#     fi
+# done
