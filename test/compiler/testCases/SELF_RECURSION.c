d487ad16cd1b2fe3340de2d6ab230fb3b153a34d
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-19 06:39:16 +0000

    fixed recursion test

--- test/compiler/testCases/SELF_RECURSION.c
+++ test/compiler/testCases/SELF_RECURSION.c
@@ -1,5 +1,6 @@
 int SELF_RECURSION(int r) /*r is number of times to recurse */
 {
-    if (r == 0) return 12345;
-    return SELF_RECURSION(r--);
+    if (r == 0)
+        return 12345;
+    return SELF_RECURSION(r - 1);
 }
