d65dca3628c3c81c1a25c696fb48bd686ebb806d
QFSW <yusuf.ismail1024@gmail.com>
2019-03-19 16:11:20 +0000

    Improved python printing and reduced the number of unecessary pass statements

--- src/ast/blocks/scopeBlock.cpp
+++ src/ast/blocks/scopeBlock.cpp
@@ -12,13 +12,18 @@ void ScopeBlock::printC(std::ostream &os) const
 
 void ScopeBlock::generatePython(std::ostream &os, PythonContext &context, int scopeDepth) const
 {
-    for(size_t i = 0; i < branches.size(); i++)
+    if (branches.size() == 0)
     {
         context.indentStream(os, scopeDepth);
-        branches[i]->generatePython(os, context, scopeDepth);
+        os << "pass";
     }
-
-    context.indentStream(os, scopeDepth);
-    os << "pass";
-    os <<std::flush;
+    else
+    {
+        for(size_t i = 0; i < branches.size(); i++)
+        {
+            context.indentStream(os, scopeDepth);
+            branches[i]->generatePython(os, context, scopeDepth);
+        }
+        os <<std::flush;
+    }
 }
