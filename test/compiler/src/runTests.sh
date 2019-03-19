d4f6649e6a5581ee9f0ed987e66591c6310739d5
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-19 03:20:58 +0000

    doesn't loop through driver files

--- test/compiler/src/runTests.sh
+++ test/compiler/src/runTests.sh
@@ -16,16 +16,23 @@ $cwd/moveTests.sh #so we can run tests from final dir for security
 
 
 for c_file in ${test_cases_dir}/*.c ; do
+    [[ $c_file == *_driver.c ]] && continue #ignore the already existing driver files
     base_name=$(echo $c_file | sed -E -e "s|${test_cases_dir}/([^.]+)[.]c|\1|g");
     driver_file="$test_cases_dir/${base_name}_driver.c"
     asm_file=$build_dir/$base_name.s
     obj_file=$build_dir/$base_name.o
     binary_out=$bin_dir/$base_name
-    
-    $c_compiler -S $c_file -o $asm_file
-    mips-linux-gnu-gcc -mfp32 -o $obj_file -c $asm_file
-    mips-linux-gnu-gcc -mfp32 -static -o $binary_out $obj_file $driver_file
-    qemu-mips $binary_out
+
+    echo $c_file
+    echo $base_name
+    echo $driver_file
+    echo $asm_file
+    echo $obj_file
+    echo $binary_out
+    # $c_compiler -S $c_file -o $asm_file
+    # mips-linux-gnu-gcc -mfp32 -o $obj_file -c $asm_file
+    # mips-linux-gnu-gcc -mfp32 -static -o $binary_out $obj_file $driver_file
+    # qemu-mips $binary_out
     
     exit_code=$?
     echo "exit code: $exit_code"
