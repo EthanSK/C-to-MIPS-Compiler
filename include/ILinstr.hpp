299a186235e3ac76e12ba3e81c9f345557076cce
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-20 19:46:37 +0000

    start ILinstr

---
+++ include/ILinstr.hpp
@@ -0,0 +1,14 @@
+#ifndef ilinstr_hpp
+#define ilinstr_hpp
+
+#include <vector>
+#include <string>
+
+
+struct ILinstr
+{
+    std::string label; //eg L1
+    std::string opcode; //eg add
+};
+
+#endif
