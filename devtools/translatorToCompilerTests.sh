2087c9a51dedbb2849c2d90630fd36d7ac80fa9a
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-20 04:26:37 +0000

    copies over tests and renames main function

--- devtools/translatorToCompilerTests.sh
+++ devtools/translatorToCompilerTests.sh
@@ -2,12 +2,16 @@
 
 
 #Notes:
+# this script implements the necessary changes between the python tester and compiler tester interfaces (eg making the exit code 0 meaning a pass for compiler tests)
 # make sure the python tests all pass first, and make sure that there are no ref errors
 
 cwd="devtools"
 python_tests_dir="test/pythonTranslator/tests"
-compiler_tests_dir="test/compiler/tests"
+compiler_tests_dir="test/compiler/tests/fromPyTrans" #have under subfolder so we don't confuse manually created tests with  auto moved ones
 c_files=$(find $python_tests_dir -name "*.c")
+
+new_main_name="mainFUNCTION"
+
 
 
 #Colours!!
@@ -23,12 +27,24 @@ white='\033[0;37m'        # White
 
 
 for c_file in $c_files ; do
-    rel_name_noext=$(echo $c_file | sed -E -e "s|${tests_dir}/([^.]+)[.]c|\1|g"); #eg functions/funcA
+    rel_name_noext=$(echo $c_file | sed -E -e "s|${python_tests_dir}/([^.]+)[.]c|\1|g"); #eg functions/funcA
     parent_dir_rel=$(dirname "${rel_name_noext}") #eg or functions/
     base_name_ext=$(basename "$c_file"); #eg funcA.c
 
+    mkdir -p  $compiler_tests_dir/$parent_dir_rel
+
+
+    # find what the exit code should be
     python3 ${python_tests_dir}/$rel_name_noext.py
-    got_py_exit_code=$?
+    exit_code_py=$?
+
+    target_c_file=$compiler_tests_dir/$rel_name_noext.c
+    # copy over c file to compiler dir
+    cp -f $c_file $target_c_file #should overwrite existing files
+
+    #change the name of the main() function to $new_main_name
+    sed -i -e "s/main/$new_main_name/g" $target_c_file
+
 
 
 done
