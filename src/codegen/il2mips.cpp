8e64867053604928ba44f813acedfebe219d2ea2
QFSW <yusuf.ismail1024@gmail.com>
2019-03-22 23:55:54 +0000

    Added nop support

--- src/codegen/il2mips.cpp
+++ src/codegen/il2mips.cpp
@@ -24,6 +24,7 @@ void IL2MIPS::convertInstr(Instr &instr, MIPSContext &context)
     else if (instr.opcode == "mov") { IL2MIPS::mov(instr, context); }
     else if (instr.opcode == "eq") { IL2MIPS::eq(instr, context); }
     else if (instr.opcode == "ne") { IL2MIPS::ne(instr, context); }
+    else if (instr.opcode == "nop") { context.addInstr(instr); }
     else if (instr.opcode == "") { context.addInstr(instr); } //for labels without opcode 
     else { throw std::string("Unexpected IL opcode " + instr.opcode + "."); }
 }
