cf6e7e4a9a8e25be32893fecb71c84a4f67e6b28
QFSW <yusuf.ismail1024@gmail.com>
2019-03-22 20:46:42 +0000

    Returns now restore the stack pointer

--- src/codegen/ILtoMIPS/retv.cpp
+++ src/codegen/ILtoMIPS/retv.cpp
@@ -2,7 +2,9 @@
 
 void IL2MIPS::retv(Instr instr, MIPSContext &context, std::vector<Instr> &mipsInstrs) 
 {
+    int stackSize = context.getAllocator().stackSize();
     mipsInstrs.push_back(Instr("mov", "$v0", instr.dest));
+    if (stackSize > 0) { mipsInstrs.push_back(Instr("addi", "$sp", std::to_string(stackSize))); }
     mipsInstrs.push_back(Instr("jr", "$ra"));
     mipsInstrs.push_back(Instr("nop"));
 }
