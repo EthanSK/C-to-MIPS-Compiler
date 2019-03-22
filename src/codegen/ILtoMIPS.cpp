61706effe96933c13826783799e8051eb871de2e
QFSW <yusuf.ismail1024@gmail.com>
2019-03-22 14:30:43 +0000

    Fixed the pseudo switch statement to not be garbage

--- src/codegen/ILtoMIPS.cpp
+++ src/codegen/ILtoMIPS.cpp
@@ -17,10 +17,10 @@ void ILtoMIPS::convertToMIPS(std::vector<ILinstr> &instrs, MIPSContext &context,
 void ILtoMIPS::convertInstr(ILinstr &instr, MIPSContext &context, std::vector<MIPSinstr> &mipsInstr)
 {
     if (instr.opcode == "fdef") { ILtoMIPS::fdef(instr, context, mipsInstr); }
-    if (instr.opcode == "fend") { ILtoMIPS::fend(instr, context, mipsInstr); }
-    if (instr.opcode == "fcall") { ILtoMIPS::fcall(instr, context, mipsInstr); }
-    if (instr.opcode == "scu") { ILtoMIPS::scu(instr, context, mipsInstr); }
-    if (instr.opcode == "scd") { ILtoMIPS::scd(instr, context, mipsInstr); }
-    if (instr.opcode == "decl") { ILtoMIPS::decl(instr, context, mipsInstr); }
+    else if (instr.opcode == "fend") { ILtoMIPS::fend(instr, context, mipsInstr); }
+    else if (instr.opcode == "fcall") { ILtoMIPS::fcall(instr, context, mipsInstr); }
+    else if (instr.opcode == "scu") { ILtoMIPS::scu(instr, context, mipsInstr); }
+    else if (instr.opcode == "scd") { ILtoMIPS::scd(instr, context, mipsInstr); }
+    else if (instr.opcode == "decl") { ILtoMIPS::decl(instr, context, mipsInstr); }
     else { throw std::string("Unexpected IL opcode " + instr.opcode + "."); }
 }
