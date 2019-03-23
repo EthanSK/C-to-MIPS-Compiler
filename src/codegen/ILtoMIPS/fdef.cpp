66298e0b3dead7c793e0d67eb664faa649537c52
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-23 15:35:26 +0000

    mov->move

--- src/codegen/ILtoMIPS/fdef.cpp
+++ src/codegen/ILtoMIPS/fdef.cpp
@@ -4,6 +4,6 @@ void IL2MIPS::fdef(Instr instr, MIPSContext &context)
 {
     context.addInstr(Instr::makeLabel(instr.dest + "()"));
     context.pushFrame();
-    context.addInstr(Instr("mov", "0_fp", "$fp"));
-    context.addInstr(Instr("mov", "0_ra", "$ra"));
+    context.addInstr(Instr("move", "0_fp", "$fp"));
+    context.addInstr(Instr("move", "0_ra", "$ra"));
 }
--- src/codegen/ILtoMIPS/ret.cpp
+++ src/codegen/ILtoMIPS/ret.cpp
@@ -3,8 +3,8 @@
 void IL2MIPS::ret(MIPSContext &context) 
 {
     int stackSize = context.stackSize();
-    context.addInstr(Instr("mov", "$fp", "0_fp"));
-    context.addInstr(Instr("mov", "$ra", "0_ra"));
+    context.addInstr(Instr("move", "$fp", "0_fp"));
+    context.addInstr(Instr("move", "$ra", "0_ra"));
     if (stackSize > 0) { context.addInstr(Instr("addi", "$sp", "$sp", std::to_string(stackSize))); }
     context.addInstr(Instr("jr", "$ra"));
     context.addInstr(Instr("nop"));
