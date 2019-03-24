392e4e9d178864efd160dbc13888e3e177edf978
QFSW <yusuf.ismail1024@gmail.com>
2019-03-24 07:20:07 +0000

    Made identifier into an rvalue

--- include/ast/identifier.hpp
+++ include/ast/identifier.hpp
@@ -3,8 +3,9 @@
 
 #include "statement.hpp"
 #include "lvalue.hpp"
+#include "rvalue.hpp"
 
-class Identifier : public Statement, public LValue
+class Identifier : public Statement, public LValue, public RValue
 {
 public:
   Identifier(std::string name);
@@ -12,6 +13,8 @@ public:
   void generatePython(std::ostream &os, PythonContext &context, int scopeDepth = 0) const override;
   void generateIL(std::vector<Instr> &instrs, ILContext &context, std::string destReg) const override;
   void generateLValueStoreIL(std::vector<Instr> &instrs, ILContext &context, std::string inputReg) const override;
+  bool isConstant() const override;
+  int evalConst() const override;
 
 protected:
   void printC(std::ostream &os) const override;
--- src/ast/identifier.cpp
+++ src/ast/identifier.cpp
@@ -26,3 +26,6 @@ void Identifier::generateLValueStoreIL(std::vector<Instr> &instrs, ILContext &co
 {
 	instrs.push_back(Instr("mov", _name, inputReg));
 }
+
+bool Identifier::isConstant() const { return "false"; }
+int Identifier::evalConst() const { throw "cannot statically evaluate an identifier"; }
