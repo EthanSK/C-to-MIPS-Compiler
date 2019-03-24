b4024c23e428a61eee65ee691f0bde7eaf18fb1b
QFSW <yusuf.ismail1024@gmail.com>
2019-03-24 15:06:03 +0000

    Added test for and short-circuiting

---
+++ test/compiler/tests/misc/shortcircuit_and.c
@@ -0,0 +1,16 @@
+int x;
+int foo(int addr, int ret)
+{
+    x += addr;
+    return ret;
+}
+
+int shortcircuit_and()
+{
+    x = 0;
+    foo(1, 1) && foo(2, 1);
+    foo(3, 1) && foo(4, 0);
+    foo(5, 0) && foo(6, 1);
+    foo(7, 0) && foo(8, 0);
+    return x;
+}
