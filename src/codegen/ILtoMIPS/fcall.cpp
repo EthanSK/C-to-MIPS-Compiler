d27ce4b6f13592fe334461dbae7386757b28c05b
QFSW <yusuf.ismail1024@gmail.com>
2019-03-24 14:22:46 +0000

    fcall now works with any number of parameters

--- src/codegen/ILtoMIPS/fcall.cpp
+++ src/codegen/ILtoMIPS/fcall.cpp
@@ -5,13 +5,17 @@ void IL2MIPS::fcall(Instr instr, MIPSContext &context)
 {
     for (int i = 0; i < instr.extraData.size(); ++i)
     {
-        if (i < 4)
-        {
-            context.addInstr(Instr("move", "$a" + std::to_string(i), instr.extraData[i]));
-        }
+        if (i < 4) { context.addInstr(Instr("move", "$a" + std::to_string(i), instr.extraData[i])); }
+        else { context.addInstr(Instr("move", "_farg" + std::to_string(i), instr.extraData[i])); }    
     }
 
-    int argFrameSize = std::max((int)instr.extraData.size(), 4) * 4;
+    for (int i = instr.extraData.size() - 1; i > 3; --i)
+    {
+        context.alloc(Allocation(4, "_arg" + std::to_string(i)));
+        context.addInstr(Instr("move", "_arg" + std::to_string(i), "_farg" + std::to_string(i)));
+    }
+
+    int argFrameSize = 4 * 4;
     context.addRawInstr(Instr("addi", "$sp", "$sp", std::to_string(-argFrameSize), {"#raw"}));
     context.addBranchInstr(Instr("jal", instr.input1), instr.label);
     context.addRawInstr(Instr("addi", "$sp", "$sp", std::to_string(argFrameSize), {"#raw"}));
