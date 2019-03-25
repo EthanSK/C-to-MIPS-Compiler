87145dd22ea1eccd88b373ececdd7336247ad258
QFSW <yusuf.ismail1024@gmail.com>
2019-03-25 09:01:56 +0000

    Fixed typo

--- src/codegen/ILtoMIPS/loc.cpp
+++ src/codegen/ILtoMIPS/loc.cpp
@@ -1,6 +1,6 @@
 #include "il2mips.hpp"
 
-void IL2MIPS::init(Instr instr, MIPSContext &context) 
+void IL2MIPS::loc(Instr instr, MIPSContext &context) 
 {
     context.loadAddress(instr.input1, instr.dest);
 }
