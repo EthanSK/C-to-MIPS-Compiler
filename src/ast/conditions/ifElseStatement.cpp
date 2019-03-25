dc79c6dd801fd1e2dad189936f53a3c1807bb312
QFSW <yusuf.ismail1024@gmail.com>
2019-03-25 07:41:50 +0000

    If else now uses $t0 for the condition

--- src/ast/conditions/ifElseStatement.cpp
+++ src/ast/conditions/ifElseStatement.cpp
@@ -40,12 +40,11 @@ void IfElseStatement::generatePython(std::ostream &os, PythonContext &context, i
 
 void IfElseStatement::generateIL(std::vector<Instr> &instrs, ILContext &context, std::string destReg) const
 {
-    std::string conditionReg = context.makeName("cond");
     std::string else_lb = context.makeLabelName("else");
     std::string endif_lb = context.makeLabelName("endif");
 
-    context.compileInput(getCondition(), instrs, conditionReg);
-    instrs.push_back(Instr("bez", else_lb, conditionReg));
+    context.compileInput(getCondition(), instrs, "$t0");
+    instrs.push_back(Instr("bez", else_lb,  "$t0"));
     getIfScopeBlock()->generateIL(instrs, context, destReg);
     instrs.push_back(Instr("b", endif_lb));
     instrs.push_back(Instr::makeLabel(else_lb));
