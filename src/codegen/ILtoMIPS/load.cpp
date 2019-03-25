ce1f75ded29af31392f8c1839c520ac539af15cb
QFSW <yusuf.ismail1024@gmail.com>
2019-03-25 09:38:24 +0000

    Extremely minimal pointers now work

--- src/codegen/ILtoMIPS/load.cpp
+++ src/codegen/ILtoMIPS/load.cpp
@@ -2,6 +2,6 @@
 
 void IL2MIPS::load(Instr instr, MIPSContext &context) 
 {
-    context.addInstr(Instr("mov", "$t0", instr.input1), instr.label);
+    context.addInstr(Instr("move", "$t0", instr.input1), instr.label);
     context.addRawInstr(Instr("lw", instr.dest, instr.input2 + "($t0)"));
 }
--- test/parser/testProgram.c
+++ test/parser/testProgram.c
@@ -1,5 +1,5 @@
 int main()
 {
-    int x = 1;
+    int x = 5;
     return *&x;
 }
