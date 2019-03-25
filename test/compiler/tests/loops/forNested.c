e5ba16a9acee6eada6142469e7d3d34ea31cba05
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-25 03:59:05 +0000

    still trynafix loop

--- test/compiler/tests/loops/forNested.c
+++ test/compiler/tests/loops/forNested.c
@@ -1,14 +1,14 @@
 int forNested()
 {
-    int sum;
+    int sum = 0;
     int i;
     for (i = 0; i < 4; ++i) 
     {
         int j;
-        for(j = 0; i < 5; i++)
+        for(j = 0; j < 5; j++)
         {
             sum++;
         }
     }
-    return i; 
+    return sum; 
 }
