ac96d413dc9a6192da349708c64e574324a88b06
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-03-25 02:57:00 +0000

    break and continue should now work

--- src/ast/keywords/breakKeyword.cpp
+++ src/ast/keywords/breakKeyword.cpp
@@ -8,6 +8,6 @@ void BreakKeyword::printC(std::ostream &os) const
 void BreakKeyword::generateIL(std::vector<Instr> &instrs, ILContext &context, std::string destReg) const
 {
     //need to jump to label of end of function, which can be found in context
-    std::string endLoopLabel = std::get<0>(context.getLastLoopLabel());
+    std::string endLoopLabel = std::get<1>(context.getLastLoopLabel());
     instrs.push_back(Instr("b", endLoopLabel));
 }
--- src/ast/keywords/continueKeyword.cpp
+++ src/ast/keywords/continueKeyword.cpp
@@ -9,6 +9,6 @@ void ContinueKeyword::printC(std::ostream &os) const
 void ContinueKeyword::generateIL(std::vector<Instr> &instrs, ILContext &context, std::string destReg) const
 {
     //need to jump to label of begining of function, which can be found in context
-    std::string endLoopLabel = std::get<1>(context.getLastLoopLabel());
+    std::string endLoopLabel = std::get<0>(context.getLastLoopLabel());
     instrs.push_back(Instr("b", endLoopLabel));
 }
