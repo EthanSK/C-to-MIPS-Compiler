d53b1d196ad4e94addd49bcd30f7490f5db8ef6c
QFSW <yusuf.ismail1024@gmail.com>
2019-03-25 10:31:06 +0000

    Optimised store mips generation

--- src/codegen/ILtoMIPS/store.cpp
+++ src/codegen/ILtoMIPS/store.cpp
@@ -2,7 +2,7 @@
 
 void IL2MIPS::store(Instr instr, MIPSContext &context) 
 {
-    context.addInstr(Instr("move", "$t0", instr.input1), instr.label);
-    context.addInstr(Instr("move", "$t1", instr.dest), instr.label);
-    context.addRawInstr(Instr("sw", "$t1", instr.input2 + "($t0)"));
+    context.addInstr(Instr("move", "$t1", instr.input1), instr.label);
+    context.addInstr(Instr("move", "$t0", instr.dest), instr.label);
+    context.addRawInstr(Instr("sw", "$t0", instr.input2 + "($t1)"));
 }
