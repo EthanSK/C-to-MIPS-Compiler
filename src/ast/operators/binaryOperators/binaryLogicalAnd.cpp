48d34b48adfd185f1f7d6d44516271ae2cfed7c5
QFSW <yusuf.ismail1024@gmail.com>
2019-03-26 13:40:14 +0000

    && operator now has short circuiting

--- src/ast/operators/binaryOperators/binaryLogicalAnd.cpp
+++ src/ast/operators/binaryOperators/binaryLogicalAnd.cpp
@@ -26,9 +26,19 @@ void BinaryLogicalAnd::generateIL(std::vector<Instr> &instrs, ILContext &context
 	std::string opcode = "andl";
 	std::string leftReg = context.makeName(opcode + "_l");
 	std::string rightReg = context.makeName(opcode + "_r");
+	std::string shortCircuit_lb = context.makeLabelName(opcode + "_sc");
+	std::string end_lb = context.makeLabelName(opcode + "_end");
+
 	context.compileInput(getLeft(), instrs, leftReg);
+	instrs.push_back(Instr("bez", shortCircuit_lb, leftReg));
+
 	context.compileInput(getRight(), instrs, rightReg);
 	instrs.push_back(Instr(opcode, destReg, leftReg, rightReg));
+	instrs.push_back(Instr("b", end_lb));
+
+	instrs.push_back(Instr::makeLabel(shortCircuit_lb));
+	instrs.push_back(Instr("li", destReg, "0"));
+	instrs.push_back(Instr::makeLabel(end_lb));
 }
 
 int BinaryLogicalAnd::evalConst() const
