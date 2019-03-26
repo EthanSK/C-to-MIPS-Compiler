b93cc8539699e0287b715d7ab3f4166e9a5c2ded
QFSW <yusuf.ismail1024@gmail.com>
2019-03-26 02:35:20 +0000

    Added some stack reference optimisations to improve readability

--- test/parser/testProgram.c
+++ test/parser/testProgram.c
@@ -1,16 +1,6 @@
-int factorial1(int n)
-{
-    if (n == 0) { return 1; }
-    else { return n * factorial2(n - 1); }
-}
+int main();
 
-int factorial2(int n)
+int main()
 {
-    if (n == 0) { return 1; }
-    else { return n * factorial1(n - 1); }
-}
-
-int mutual_recursion()
-{
-    return factorial1(5);
+    return 0;
 }
