48bef360fe7e71dc7b97591b3aeca28d44b714b3
QFSW <yusuf.ismail1024@gmail.com>
2019-03-26 18:55:03 +0000

    Fixed post inc and dec to not use a temporary

--- src/ast/operators/unaryOperators/unaryPostDecrement.cpp
+++ src/ast/operators/unaryOperators/unaryPostDecrement.cpp
@@ -9,11 +9,12 @@ void UnaryPostDecrement::printC(std::ostream &os) const
 
 void UnaryPostDecrement::generateIL(std::vector<Instr> &instrs, ILContext &context, std::string destReg) const
 {
-    getOperand()->generateIL(instrs, context, "$t0");
-	instrs.push_back(Instr("mov", destReg, "$t0"));
-	instrs.push_back(Instr("dec", "$t0"));
+	std::string storageLabel = context.makeName("dec_t");
+    getOperand()->generateIL(instrs, context, storageLabel);
+	instrs.push_back(Instr("mov", destReg, storageLabel));
+	instrs.push_back(Instr("dec", storageLabel));
 	LValuePtr lvalue = Utils::tryCast<LValue>(getOperand(), "Illegal ++: " + toString() + ". LHS of an increment must be an lvalue");
-	lvalue->generateLValueStoreIL(instrs, context, "$t0");
+	lvalue->generateLValueStoreIL(instrs, context, storageLabel);
 }
 
 bool UnaryPostDecrement::isConstant() const { return false; }
--- src/ast/operators/unaryOperators/unaryPostIncrement.cpp
+++ src/ast/operators/unaryOperators/unaryPostIncrement.cpp
@@ -9,11 +9,12 @@ void UnaryPostIncrement::printC(std::ostream &os) const
 
 void UnaryPostIncrement::generateIL(std::vector<Instr> &instrs, ILContext &context, std::string destReg) const
 {
-    getOperand()->generateIL(instrs, context, "$t0");
-	instrs.push_back(Instr("mov", destReg, "$t0"));
-	instrs.push_back(Instr("inc", "$t0"));
+	std::string storageLabel = context.makeName("inc_t");
+    getOperand()->generateIL(instrs, context, storageLabel);
+	instrs.push_back(Instr("mov", destReg, storageLabel));
+	instrs.push_back(Instr("inc", storageLabel));
 	LValuePtr lvalue = Utils::tryCast<LValue>(getOperand(), "Illegal ++: " + toString() + ". LHS of an increment must be an lvalue");
-	lvalue->generateLValueStoreIL(instrs, context, "$t0");
+	lvalue->generateLValueStoreIL(instrs, context, storageLabel);
 }
 
 bool UnaryPostIncrement::isConstant() const { return false; }
